50c6e61da27975afa47d556d20e8c875b9c1b5bc
icsy7867 <wstewart3@gmail.com>
2021-08-24 17:42:48 -0400

    Updated server IP Tables
    Updated to use arrays and loops to do more cleanly.

--- server_IPTables.sh
+++ server_IPTables.sh
@@ -1,46 +1,26 @@
 #!/bin/bash
-# venet0 is the externally facing interface on your VPS (Could be eth0 for example)
-# tun0 is the name of your openvpn tunnel
-# 10.8.0.6 is the IP Address of your internal NAT Openvpn Client.
-# 10.8.0.1 is the openvpn IP address of the openvpn server
+#echo 1 > /proc/sys/net/ipv4/ip_forward
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
+interfaceName="eth0"
+tunInterface="tun0"
+clientIP="192.168.255.6"
+serverIP="192.168.255.1"
 
-iptables -A INPUT -i venet0 -p tcp --dport 22 -j ACCEPT   # Accept SSH
-iptables -A INPUT -i venet0 -p udp --dport 1194 -j ACCEPT # Accept OpenVPN
-iptables -A INPUT -i venet0 -j DROP                       # drop everything else
+declare -a ports
+ports=( 443 32400 )
+
 
 ##Allow crosstalk
-iptables -A FORWARD -i tun0 -o venet0 -j ACCEPT
-iptables -A FORWARD -i venet0 -o tun0 -j ACCEPT
-iptables -A FORWARD -i venet0 -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -i tun0 -o venet0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i ${tunInterface} -o ${interfaceName} -j ACCEPT
+iptables -A FORWARD -i ${interfaceName} -o ${tunInterface} -j ACCEPT
+iptables -A FORWARD -i ${interfaceName} -o ${tunInterface} -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i ${tunInterface} -o ${interfaceName} -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+for port in "${ports[@]}"
 
-## Send docker webservers
-iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 443 -j DNAT --to-destination 10.8.0.6:443
-iptables -t nat -A POSTROUTING -p tcp -d 10.8.0.6 --dport 443 -j SNAT --to-source 10.8.0.1
+do
+  	echo $port
+        iptables -t nat -A PREROUTING -i ${interfaceName} -p tcp --dport $port -j DNAT --to-destination ${clientIP}:$port
+        iptables -t nat -A POSTROUTING -p tcp -d ${clientIP} --dport $port -j SNAT --to-source ${serverIP}
 
-## Send docker webservers
-iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 80 -j DNAT --to-destination 10.8.0.6:80
-iptables -t nat -A POSTROUTING -p tcp -d 10.8.0.6 --dport 80 -j SNAT --to-source 10.8.0.1
-
-## Emby
-iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 8920 -j DNAT --to-destination 10.8.0.6:8920
-iptables -t nat -A POSTROUTING -p tcp -d 10.8.0.6 --dport 8920 -j SNAT --to-source 10.8.0.1
-
-## openvpn
-iptables -t nat -A PREROUTING -i venet0 -p udp --dport 1194 -j DNAT --to-destination 10.8.0.6:1194
-iptables -t nat -A POSTROUTING -p udp -d 10.8.0.6 --dport 1194 -j SNAT --to-source 10.8.0.1
-
-## minecraft PE
-iptables -t nat -A PREROUTING -i venet0 -p udp --dport 19132 -j DNAT --to-destination 10.8.0.6:19132
-iptables -t nat -A POSTROUTING -p udp -d 10.8.0.6 --dport 19132 -j SNAT --to-source 10.8.0.1
-
-## RDP
-iptables -t nat -A PREROUTING -i venet0 -p tcp --dport 3389 -j DNAT --to-destination 10.8.0.6:3389
-iptables -t nat -A POSTROUTING -p tcp -d 10.8.0.6 --dport 3389 -j SNAT --to-source 10.8.0.1
-
-## RDP
-iptables -t nat -A PREROUTING -i venet0 -p udp --dport 3389 -j DNAT --to-destination 10.8.0.6:3389
-iptables -t nat -A POSTROUTING -p udp -d 10.8.0.6 --dport 3389 -j SNAT --to-source 10.8.0.1
+done
